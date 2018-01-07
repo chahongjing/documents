f86be6490134da3729ff83960b56499f8fff3619
chahongjing <310510906@qq.com>
2018-01-07 16:21:13 +0800

    添加oracle驱动安装到本地maven库

---
+++ java/mvn_oracle_driver/install_oracle_driver.bat
@@ -0,0 +1,3 @@
+:: 如果bat和jar包在同一目录 -Dfile可以直接写包名，如-Dfile=ojdbc6.jar；如果写路径，注意路径中汉字，有可能会有乱码问题
+:: jar包位置一般在oracle安装目录中dbhome/jdbc/lib/ojdbc6.jar
+start mvn install:install-file -Dpackaging=jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dfile=F:\Source\Java\ToolSiteMvc4J\trunk\lib\ojdbc6.jar -Dversion=11.2.0
