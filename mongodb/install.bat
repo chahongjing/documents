d1107868cd5de3d089bdf2a0f743d66ce56b021f
chahongjing <310510906@qq.com>
2018-02-12 14:12:38 +0800

    mongodb文档完善

--- mongodb/install.bat
+++ mongodb/install.bat
@@ -1,4 +1,4 @@
-mongod.exe --logpath F:\software\mongodb3.6\bin\log\mongodb.log --logappend --dbpath F:\software\mongodb3.6\bin\data --directoryperdb --serviceName MongoDB --install
+mongod.exe --logpath F:\software\mongodb3.6\bin\log\mongodb.log --logappend --dbpath F:\software\mongodb3.6\bin\data --directoryperdb --serviceName MongoDB --install --bind_ip 0.0.0.0
 
 rem remove service
 rem mongod.exe --remove --serviceName "MongoDB"
