337a1b748aa4d7bb1eb7d768b11d4a0b34683d0f
chahongjing <310510906@qq.com>
2018-03-07 20:15:32 +0800

    文档完善

--- oracle/oracle导入.bat
+++ oracle/oracle导入.bat
@@ -1,2 +1,6 @@
+rem development db import
 imp libra/libra$20167@127.0.0.1:1521/orcl file=d:\201803071715.dmp full=y
+
+rem test db import
+rem imp LIBRA_TEST/LIBRA2017@172.16.16.202:1521/LIBRA file=d:\201803072006.dmp full=y
 pause;
--- oracle/oracle导出.bat
+++ oracle/oracle导出.bat
@@ -1,2 +1,6 @@
+rem development db export
 exp libra/libra$20167@127.0.0.1:1521/orcl file=d:\201803071715.dmp
+
+rem test db export
+rem exp LIBRA_TEST/LIBRA2017@172.16.16.202:1521/LIBRA file=d:\201803072006.dmp
 pause;
