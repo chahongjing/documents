a4a7ec85a4ea40adfeb96274fda80cddae5e3831
chahongjing <310510906@qq.com>
2018-03-20 16:54:06 +0800

    bat完善

--- oracle/oracle导入.bat
+++ oracle/oracle导入.bat
@@ -1,3 +1,4 @@
+title oracle import
 rem development db import
 imp libra/libra$20167@127.0.0.1:1521/orcl file=d:\201803071715.dmp full=y
 
--- oracle/oracle导出.bat
+++ oracle/oracle导出.bat
@@ -1,3 +1,4 @@
+title oracle export
 rem development db export
 exp libra/libra$20167@127.0.0.1:1521/orcl file=d:\201803071715.dmp
 
