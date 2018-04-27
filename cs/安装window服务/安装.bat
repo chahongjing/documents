9552438f32a2d94c91fe044846618ace5c75897d
chahongjing <310510906@qq.com>
2018-04-27 15:03:26 +0800

    文档优化

--- cs/安装window服务/安装.bat
+++ cs/安装window服务/安装.bat
@@ -3,8 +3,8 @@
 cd C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319
 cd C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727
 
-rem 卸载
+rem 卸载 注意路径要添加引号，否则有可能安装报错
 InstallUtil /u "E:\TDDOWNLOAD\FileMonitorService\bin\Debug\FileMonitorService.exe"
-rem 安装
+rem 安装 注意路径要添加引号，否则有可能安装报错
 InstallUtil "E:\TDDOWNLOAD\FileMonitorService\bin\Debug\FileMonitorService.exe"
 
