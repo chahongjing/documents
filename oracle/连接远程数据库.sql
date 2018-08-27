e8d38b572906025ff8bacd7303f484eac921dacf
chahongjing <310510906@qq.com>
2018-08-27 10:11:15 +0800

    添加

---
+++ oracle/连接远程数据库.sql
@@ -0,0 +1,13 @@
+﻿drop public database link testlibra;
+create public database link testlibra
+connect to libra2 identified by libra2
+using '(DESCRIPTION =
+(ADDRESS_LIST =
+(ADDRESS = (PROTOCOL = TCP)(HOST = 172.16.16.202)(PORT = 1521))
+)
+(CONNECT_DATA =
+(ORACLE_SID=libra)
+)
+)';
+
+seLect * from tk_shijuan@TESTLIBRA
