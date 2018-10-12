524546a63c59cdabef8ec66d9c4823af49afaa3d
chahongjing <310510906@qq.com>
2018-10-12 17:09:20 +0800

    文档整理

--- cs/注册framework.bat
+++ cs/注册framework.bat
@@ -1,3 +1,6 @@
 title register framework
+rem 32
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_regiis.exe -i
+rem 64
+rem C:\Windows\Microsoft.NET\\Framework64\v4.0.30319\aspnet_regiis.exe -i
 pause;
