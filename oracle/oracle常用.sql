fdec796ba3f4a14191c6c2698754bb7ee38b8b50
chahongjing <310510906@qq.com>
2018-10-21 19:54:05 +0800

    完善笔记

---
+++ oracle/oracle常用.sql
@@ -0,0 +1,17 @@
+﻿-- 备份mytable表为Ϊmybaktable
+create table mybaktable as
+select id, name from mytable;
+-- merge into
+merge into a
+using (select * from bbb) b
+   on (a.id = b.id)
+ when matched then 
+update set a.name = b.name
+;
+-- 递归
+SELECT connect_by_isleaf, connect_by_root a.mingcheng as root, sys_connect_by_path(a.mingcheng, '-->'),a.mingcheng
+  FROM RW_ZHENGTILANTUMINGXI a
+ WHERE a.LANTUID = 91
+ START WITH a.SHANGCENGID is NULL or a.SHANGCENGID = 0
+CONNECT BY PRIOR a.LANTUDUIXIANGID = a.SHANGCENGID
+ order siblings by a.xuhao
