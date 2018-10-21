7aecd82c74a05f2c524d9a42beeb4576995a2d07
chahongjing <310510906@qq.com>
2018-10-21 19:45:24 +0800

    完善笔记

--- oracle/merge_connectby.sql
+++ oracle/merge_connectby.sql
@@ -21,16 +21,19 @@ select a.id, a.name, b.name
   from a
  inner join b on a.id = b.id;
 
-merge into a using b on (a.id = b.id)
+-- merge into
+merge into a
+using b
+on (a.id = b.id)
 when matched then 
-update  set a.name = b.name
+update set a.name = b.name
 ;
 
 drop table b
 
 
 
---
+-- 递归
 SELECT connect_by_isleaf, connect_by_root a.mingcheng as root, sys_connect_by_path(a.mingcheng, '-->'),a.mingcheng  ,b.MINGCHENG as nenglimingcheng,c.MINGCHENG as zhishidianmingcheng,d.MINGCHENG as tixingmingcheng
 FROM RW_ZHENGTILANTUMINGXI a
 LEFT JOIN XK_NENGLICENGJI b on a.nengliid = b.nengliid
@@ -39,4 +42,4 @@ LEFT JOIN XK_TIXING d on a.tixingid = d.tixingid
 WHERE a.LANTUID = 91
 START WITH a.SHANGCENGID is NULL or a.SHANGCENGID = 0
 CONNECT BY PRIOR a.LANTUDUIXIANGID = a.SHANGCENGID
-order siblings  by a.xuhao
+order siblings by a.xuhao
