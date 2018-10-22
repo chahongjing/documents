e4845639d6c87cb87f1450ff0b106831644f1286
chahongjing <310510906@qq.com>
2018-10-22 16:43:22 +0800

    完善sql

--- oracle/oracle常用.sql
+++ oracle/oracle常用.sql
@@ -1,17 +1,29 @@
 ﻿-- 备份mytable表为Ϊmybaktable
 create table mybaktable as
 select id, name from mytable;
+
 -- merge into
 merge into a
 using (select * from bbb) b
    on (a.id = b.id)
  when matched then 
-update set a.name = b.name
-;
+update set a.name = b.name;
+
 -- 递归
 SELECT connect_by_isleaf, connect_by_root a.mingcheng as root, sys_connect_by_path(a.mingcheng, '-->'),a.mingcheng
   FROM RW_ZHENGTILANTUMINGXI a
  WHERE a.LANTUID = 91
  START WITH a.SHANGCENGID is NULL or a.SHANGCENGID = 0
 CONNECT BY PRIOR a.LANTUDUIXIANGID = a.SHANGCENGID
- order siblings by a.xuhao
+ order siblings by a.xuhao;
+
+-- 使用块，注意定义的变量不能和字段重名，否则会把所有的数据都更新了
+declare
+  myxuekeid number(19, 0);
+begin
+  myxuekeid := 1014783074325630976;
+  delete from ym_renwuzu where renwuzuid in (
+      select renwuzu.renwuzuid from ym_renwuzu renwuzu where renwuzu.xuekeid = myxuekeid
+  );
+end;
+-- commit;
