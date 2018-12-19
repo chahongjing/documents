6434937e8da3d984087c7356c83148409640256b
chahongjing <310510906@qq.com>
2018-12-19 11:33:27 +0800

    添加发布批处理文件

---
+++ tomcat/copyFiles.bat
@@ -0,0 +1,45 @@
+@echo off
+title deploy ToolSiteMvc4J and myvue
+
+set javaDir=F:\MyWorkplace\JavaCode\ToolSiteMvc4J\trunk\web\target\ToolSiteMvc4J\
+set vueDir=D:\webvue\
+set targetDir=F:\CompanyWorkplace\apache-tomcat-9.0.8_service\webapps\ToolSiteMvc4J\
+set configDir=F:\CompanyWorkplace\apache-tomcat-9.0.8_service\webapps\static\
+
+set /p type=deploy type: all(1), java(2), vue(3):
+if "%type%"=="1" (
+goto copyJava
+)
+if "%type%"=="2" (
+goto copyJava
+)
+if "%type%"=="3" (
+goto copyVue
+)
+echo.
+echo wrong type
+goto end
+
+:copyJava
+echo.
+echo copy ToolSiteMvc4J
+xcopy %javaDir%*.* %targetDir% /s/e/y
+if "%type%"=="1" (
+goto copyVue
+)
+goto end
+
+:copyVue
+echo.
+echo delete static
+rmdir /s/q %targetDir%static
+echo.
+echo copy myvue
+xcopy %vueDir%*.* %targetDir% /s/e/y
+echo.
+echo copy config
+xcopy %configDir%*.* %targetDir%static\ /s/e/y
+
+:end
+echo.
+pause;
