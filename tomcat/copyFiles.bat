ba9806c4201bf67babbc141312ed666a4c116607
chahongjing <310510906@qq.com>
2018-12-24 21:47:15 +0800

    完善命令

--- tomcat/copyFiles.bat
+++ tomcat/copyFiles.bat
@@ -1,10 +1,10 @@
 @echo off
 title deploy ToolSiteMvc4J and myvue
 
-set javaDir=F:\MyWorkplace\JavaCode\ToolSiteMvc4J\trunk\web\target\ToolSiteMvc4J\
+set javaDir=F:\Source\Java\ToolSiteMvc4J\trunk\web\target\ToolSiteMvc4J\
 set vueDir=D:\webvue\
-set targetDir=F:\CompanyWorkplace\apache-tomcat-9.0.8_service\webapps\ToolSiteMvc4J\
-set configDir=F:\CompanyWorkplace\apache-tomcat-9.0.8_service\webapps\static\
+set targetDir=E:\devsoft\java\apache-tomcat-9.0.8_service\webapps\ToolSiteMvc4J\
+set configDir=E:\devsoft\java\apache-tomcat-9.0.8_service\static\
 
 echo.
 set /p type=deploy type: all(1), java(2), vue(3):
@@ -22,12 +22,18 @@ echo wrong type
 goto end
 
 :copyJava
+echo stop tomcat service
+net stop tomcat9
 echo.
 echo delete ToolSiteMvc4J
 rmdir /s/q %targetDir%
 echo.
 echo copy ToolSiteMvc4J
 xcopy %javaDir%*.* %targetDir% /s/e/y
+
+echo.
+echo start tomcat service
+net start tomcat9
 if "%type%"=="1" (
 goto copyVue
 )
