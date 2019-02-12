7ae6a384f505456c2ddbb28b4d1de79838b54fe2
chahongjing <310510906@qq.com>
2019-02-12 11:40:11 +0800

    添加oracle相关

---
+++ oracle/用户_创建表空间.sql
@@ -0,0 +1,21 @@
+-- 创建表空间
+create tablespace LIBRA_test
+logging
+datafile 'E:\db_home1\LIBRA_test.dbf'
+size 32m
+autoextend on
+next 32m maxsize 2048m
+extent management local;
+-- 删除表空间
+-- drop tablespace LIBRA_test;
+-- 查询当前用户表空间
+-- select username,default_tablespace from user_users;
+
+-- 查询当前实例
+show parameter instance
+-- 查询当前实例名
+select * from v$database
+-- 当前用户表空间
+select * from dba_users where upper(username) = upper('LIBRA2_DEV');
+-- 所有表空间
+select * from v$tablespace;
